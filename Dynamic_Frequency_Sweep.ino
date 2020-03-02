362b8b4b5a65a6c3562f744e4459264cdfdf63df
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2020-03-02 23:20:39 +0800

    Add files via upload

---
+++ Dynamic_Frequency_Sweep.ino
@@ -0,0 +1,35 @@
+int relayPin = 8;
+int delayTime;
+int LEDpin = 9;
+int maxfrequency;
+int timePerFrequency;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("Solid State Relay ");
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(LEDpin, HIGH);
+
+}
+
+
+void loop() {
+  maxfrequency = 30; ///////CHANGE MAX FREQUENCY HERE NOTHING ELSE TO BE CHANGED
+  timePerFrequency = 10;
+  for (int i = 1; i <=maxfrequency; i++) {
+    for (int j = 1; j <= timePerFrequency * i; j++) {
+      delayTime = 500/i;
+      Serial.println(i);
+      digitalWrite(relayPin, LOW);
+      //Serial.println("Relay ON ");
+      
+      delay(delayTime);
+
+
+      digitalWrite(relayPin, HIGH);
+      //Serial.println("Relay OFF ");
+      delay(18);
+    }
+  }
+}
